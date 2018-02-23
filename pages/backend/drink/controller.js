13cd4d8435c2fe55c8294a008b3b4aa81eab9656
aungithub <32588766+aungithub@users.noreply.github.com>
2018-02-23 13:54:28 +0700

    add update unit detail

--- pages/backend/drink/controller.js
+++ pages/backend/drink/controller.js
@@ -13,6 +13,7 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 	$scope.selectedDrinkObject = null;
 	$scope.selectedUnitObject = null;
 	$scope.listUnitObject = null;
+	$scope.listUnitDetailObject = null;
 	$scope.selectedVendorObject = null;
 	$scope.listVendorObject = null;
 	$scope.selectedVendorDrinkObject = null;
@@ -56,6 +57,7 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 
 					if (result.data.status == 200) {
 						$scope.listDrinkObject = result.data.drink;
+						$scope.listUnitDetailObject = result.data.unitdetail_list;
 						//console.log(listDrinkObject);
 					}
 					else {
@@ -198,12 +200,13 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 			drink_order_point = $("#add_drink_order_point").val(),
 			drink_number = $("#add_drink_number").val(),
 			drink_unit_id = $("#add_drink_unit_id").val(),
+			drink_unitdetail_id = $("#add_drink_unitdetail_id").val(),
 			drink_unit_price = $("#add_drink_unit_price").val(),
 			drink_status_id = $("#add_drink_status_id").val();
 
-		if (drink_name != '' && $scope.addDrinkObject.length > 0 && drink_order_point != '' && drink_number != '' && drink_unit_id != '' && drink_unit_price != '' && drink_status_id != 999 ) {
+		if (drink_name != '' && $scope.addDrinkObject.length > 0 && drink_order_point != '' && drink_number != '' && drink_unit_id != '' && drink_unitdetail_id != '' && drink_unit_price != '' && drink_status_id != 999 ) {
 			
-			DrinkService.addDrink($("#add_drink_name").val(), $scope.addDrinkObject, drink_order_point, drink_number, drink_unit_id, drink_unit_price, drink_status_id).then(function (result) {
+			DrinkService.addDrink($("#add_drink_name").val(), $scope.addDrinkObject, drink_order_point, drink_number, drink_unit_id, drink_unitdetail_id, drink_unit_price, drink_status_id).then(function (result) {
 				if (result.data.status == 200) {
 					noty({
 		                type : 'success',
@@ -317,6 +320,7 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 							}
 							setTimeout(function() {
 								$("#edit_drink_unit_id").val($scope.selectedDrinkObject.drink_unit_id);
+								$("#edit_drink_unitdetail_id").val($scope.selectedDrinkObject.unitdetail_id);
 								$("#edit_drink_unit_price").val($scope.selectedDrinkObject.drink_unit_price);
 								//$("#edit_drink_vendor_id").val($scope.selectedDrinkObject.drink_vendor_id);
 								if ($scope.selectedDrinkVendorListObject.length > 0) {
@@ -357,11 +361,12 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 			drink_order_point = $("#edit_drink_order_point").val(),
 			drink_number = $("#edit_drink_number").val(),
 			drink_unit_id = $("#edit_drink_unit_id").val(),
+			drink_unitdetail_id = $("#edit_drink_unitdetail_id").val(),
 			drink_unit_price = $("#edit_drink_unit_price").val(),
 			drink_status_id = $("#edit_drink_status_id").val();
 
-		if (drink_name != '' && $scope.selectedDrinkVendorListObject.length > 0 && drink_order_point != '' && drink_number != '' && drink_unit_id != '' && drink_unit_price != '' && drink_status_id != 999 ) {
-			DrinkService.updateDrink(id, drink_name, $scope.selectedDrinkVendorListObject, drink_number, drink_order_point, drink_unit_id, drink_unit_price, drink_status_id).then(function (result) {
+		if (drink_name != '' && $scope.selectedDrinkVendorListObject.length > 0 && drink_order_point != '' && drink_number != '' && drink_unit_id != '' && drink_unitdetail_id != '' && drink_unit_price != '' && drink_status_id != 999 ) {
+			DrinkService.updateDrink(id, drink_name, $scope.selectedDrinkVendorListObject, drink_number, drink_order_point, drink_unit_id, drink_unitdetail_id, drink_unit_price, drink_status_id).then(function (result) {
 				if (result.data.status == 200) {
 					noty({
 		                type : 'success',
@@ -891,11 +896,12 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 	};
 
 	//cm เพิ่มเครื่องดื่ม
-	this.addDrink = function (drink_name, add_drink_object, drink_order_point, drink_number, drink_unit_id, drink_unit_price, drink_status_id) {
+	this.addDrink = function (drink_name, add_drink_object, drink_order_point, drink_number, drink_unit_id, drink_unitdetail_id, drink_unit_price, drink_status_id) {
 		return $http.post('http://localhost/restaurant-api/api_add_drink.php', {
             'drink_name' : drink_name,
             'add_drink_object' : add_drink_object,
             'drink_unit_id' : drink_unit_id,
+            'drink_unitdetail_id' : drink_unitdetail_id,
             'drink_order_point' : drink_order_point,
             'drink_number' : drink_number,
             'drink_status_id' : drink_status_id,
@@ -918,7 +924,7 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
 	};
 
 	//cm อัพเดทเครื่องดื่ม
-	this.updateDrink = function (drink_id, drink_name, edit_drink_object, drink_number, drink_order_point, drink_unit_id, drink_unit_price, drink_status_id) {
+	this.updateDrink = function (drink_id, drink_name, edit_drink_object, drink_number, drink_order_point, drink_unit_id, drink_unitdetail_id, drink_unit_price, drink_status_id) {
 		return $http.post('http://localhost/restaurant-api/api_update_drink.php', {
             'drink_id' : drink_id,
             'drink_name' : drink_name,
@@ -926,6 +932,7 @@ angular.module('RESTAURANT.admin_drink', ['ngRoute'])
             'drink_number' : drink_number, 
             'drink_order_point' : drink_order_point,
             'drink_unit_id' : drink_unit_id,
+            'drink_unitdetail_id' : drink_unitdetail_id,
             'drink_status_id' : drink_status_id,
             'drink_unit_price' : drink_unit_price
            
